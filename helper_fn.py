c6ebc0b34c35b6dee74cf910cc11f01b6d5dea2b
Vladimir Lazunin <vladimir@ddd.co.jp>
2019-12-17 09:21:59 +0900

    Fixed wrong split at line beginnings.

--- helper_fn.py
+++ helper_fn.py
@@ -8,6 +8,7 @@ def is_op(left, right, sep):
     '''
     Check if it was split inside a multi-character operator.
     '''
+    if len(left.strip()) == 0: return False
     return ((left[-1:] + sep) in multi) or ((left[-2:] + sep) in multi) or ((left[-1:] + sep + right[:1]) in multi) or ((sep + right[:1]) in multi) or ((sep + right[:2]) in multi)
     
     
